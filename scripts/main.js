4e5b11d12d1a468e8f3e22c8e063722dfa098477
Connor McCarty <connor.mccarty.cm@gmail.com>
2018-03-26 19:20:22 -0400

    Fixed question amounts, and various bugs

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,50 @@
 
 document.getElementById('start').addEventListener('click', getData);
+var amounts;
 
-function createQuestion(question){
-  let answers = [
-    {text: question.correct_answer, correct: true},
-    {text: question.incorrect_answers[0], correct: false},
-    {text: question.incorrect_answers[1], correct: false},
-    {text: question.incorrect_answers[2], correct: false}
-  ];
-  var randomAnswers = shuffle(answers);
-  let answerbuttons = randomAnswers.map(function(answer){
-    if (answer.correct === true){
-      return `<p class="options"> <input class="correct" name="${question.question}" type="radio">${answer.text}</input> </p>
-      `
+function createQuestion(question, answerType){
+  if(answerType === "multiple"){
+    let answers = [
+      {text: question.correct_answer, correct: true},
+      {text: question.incorrect_answers[0], correct: false},
+      {text: question.incorrect_answers[1], correct: false},
+      {text: question.incorrect_answers[2], correct: false}
+    ];
+    var randomAnswers = shuffle(answers);
+    let answerbuttons = randomAnswers.map(function(answer){
+      if (answer.correct === true){
+        return `<p class="options"> <input class="correct" name="${question.question}" type="radio">${answer.text}</input> </p>
+        `
+      }
+      return `<p class="options"> <input class="incorrect" name="${question.question}" type="radio">${answer.text}</input> </p>`
+    })
+      let template = ` <p class="question"> ${question.question} </p> <br/> ${answerbuttons.join(' ')}`;
+      return template;
     }
-    return `<p class="options"> <input class="incorrect" name="${question.question}" type="radio">${answer.text}</input> </p>`
-  })
-    let template = ` <p class="question"> ${question.question} </p> <br/> ${answerbuttons.join(' ')}`;
-    return template;
 }
-
 function getData(){
   let difficulty = document.getElementById('difficulty').value;
   let type = document.getElementById('type').value;
-  let amount = document.getElementById('amount').value;
   let category = document.getElementById('category').value;
+  amounts = document.getElementById('amounts').value;
   if(difficulty !== "easy" && difficulty !== "medium" && difficulty !== "hard"){
     throw Error(`Bad Arguments: ${difficulty}`);
   }
   else if(type !== "multiple" && type !== "boolean"){
     throw Error(`Bad Arguments: ${type}`);
   }
-  else if(amount < 1 || amount > 50){
+  else if(amounts < 1 || amounts > 50){
     throw Error(`Bad Arguments: Outside the range`);
   }
-  let dataFile = `https://opentdb.com/api.php?amount=${amount}&category=${category}&difficulty=${difficulty}&type=${type}`;
+  let dataFile = `https://opentdb.com/api.php?amount=${amounts}&category=${category}&difficulty=${difficulty}&type=${type}`;
   fetch(dataFile)
     .then(function(response){
       return(response.json());
     })
     .then(function(data){
       console.log(data);
-      let answerhtml = data.results.map(createQuestion);
+      const createTypedQuestion = result => createQuestion(result, type)
+      let answerhtml = data.results.map(createTypedQuestion);
       let template = answerhtml.join(`<br/> <br/>`);
       template += `<button type="button" class="button" id="submit">Submit</button>`
       document.getElementById('content').innerHTML = template;
@@ -53,17 +56,12 @@ function getData(){
 function submitAnswers(){
   let guesses = document.querySelectorAll(".options input");
   let correct = 0;
-  var amount;
-  element = document.getElementById('amount');
-  if (element !== null){
-    amount = element.value;
-  }
   for (var i= 0; i < guesses.length; i++){
     if (guesses[i].className === "correct" && guesses[i].checked) {
       correct++;
     }
   }
-  let score = (correct/amount)*100;
+  let score = (correct/amounts)*100;
   console.log(score);
   document.getElementById('content').innerHTML = document.getElementById('content').innerHTML
   + ` <br/> <p> Your score is ${score}%! <p> <button type="button" class="button" onclick="getData()">
